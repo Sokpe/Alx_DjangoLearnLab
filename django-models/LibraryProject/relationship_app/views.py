394c7aaab1265f5647a001b072e36006fe07697e
Kay <152438149+Sokpe@users.noreply.github.com>
2025-09-05 13:22:16 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,42 +1,15 @@
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
+from .models import Library
+from django.views.generic.detail import DetailView
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.views import LoginView, LogoutView
-
-class CustomLoginView(LoginView):
-    template_name = 'relationship_app/login.html'
-    form_class = AuthenticationForm
-    redirect_authenticated_user = True
-
-def register(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('list_books')
-    else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
-
-class CustomLogoutView(LogoutView):
-    template_name = 'relationship_app/logout.html'
-
-
-relationship_app/urls.py
-
-from django.urls import path
-from .views import CustomLoginView, register, CustomLogoutView
-from . import views
-
-urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
-]
+# Class-based view to display library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
