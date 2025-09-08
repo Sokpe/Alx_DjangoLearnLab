c3acb4e02098bfedf403f5028dedf5471a248f17
Kay <152438149+Sokpe@users.noreply.github.com>
2025-09-08 09:22:20 +0000

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,12 +3,7 @@ from .models import Book
 from .forms import BookForm, ExampleForm
 from django.contrib.auth.decorators import permission_required
 
-# Existing views...
-@permission_required('bookshelf.can_view', raise_exception=True)
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'bookshelf/book_list.html', {'books': books})
-
+# Example view using ExampleForm
 def example_view(request):
     if request.method == 'POST':
         form = ExampleForm(request.POST)
@@ -16,7 +11,13 @@ def example_view(request):
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
             message = form.cleaned_data['message']
-            # Process the data
+            # Process the form data
     else:
         form = ExampleForm()
     return render(request, 'bookshelf/example_form.html', {'form': form})
+
+# Other views...
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
