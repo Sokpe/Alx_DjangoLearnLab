da9a9662194fc09e3d6f5ade8506ae0a2b52d2e4
Kay <152438149+Sokpe@users.noreply.github.com>
2025-09-08 09:17:09 +0000

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,8 +1,17 @@
-from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 from .models import Book
+from .forms import BookForm
+from django.contrib.auth.decorators import permission_required
 
-@permission_required('relationship_app.can_view', raise_exception=True)
+@permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'relationship_app/book_list.html', {'books': books})
+    books = Book.objects.all()  # Using Django's ORM
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
+def search_books(request):
+    query = request.GET.get('q')
+    if query:
+        books = Book.objects.filter(title__icontains=query)  # Safe ORM query
+    else:
+        books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
