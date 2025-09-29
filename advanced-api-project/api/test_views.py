2a7adf6b85a4a1bee7b43a6144c40cf75e9aac79
Kay <152438149+Sokpe@users.noreply.github.com>
2025-09-29 13:59:40 +0000

    Update test_views.py

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -8,7 +8,7 @@ from .models import Book
 class BookAPITestCase(APITestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='password')
-        self.client.force_authenticate(user=self.user)
+        self.client.login(username='testuser', password='password')
 
     def test_create_book(self):
         # Test creating a book
@@ -19,28 +19,11 @@ class BookAPITestCase(APITestCase):
         self.assertEqual(response.data['title'], 'Test Book')
         self.assertEqual(response.data['author'], 'Test Author')
 
-    def test_update_book(self):
-        # Test updating a book
-        book = Book.objects.create(title='Test Book', author='Test Author')
-        url = reverse('book-detail', kwargs={'pk': book.pk})
-        data = {'title': 'Updated Test Book', 'author': 'Test Author'}
-        response = self.client.put(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], 'Updated Test Book')
-        self.assertEqual(response.data['author'], 'Test Author')
+    # Other tests...
 
-    def test_get_book(self):
-        # Test getting a book
-        book = Book.objects.create(title='Test Book', author='Test Author')
-        url = reverse('book-detail', kwargs={'pk': book.pk})
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['title'], 'Test Book')
-        self.assertEqual(response.data['author'], 'Test Author')
 
-    def test_unauthenticated_user(self):
-        # Test unauthenticated user
-        self.client.force_authenticate(user=None)
-        url = reverse('book-list')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+Alternatively, you can use self.client.force_authenticate if you're using token-based authentication:
+
+def setUp(self):
+    self.user = User.objects.create_user(username='testuser', password='password')
+    self.client.force_authenticate(user=self.user)
