4d10c73218d3aabe3aea63a4d89f7a7ea4fe0e0d
Kay <152438149+Sokpe@users.noreply.github.com>
2025-10-01 22:45:36 +0000

    Add files via upload

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
     
 ]
 
