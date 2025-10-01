26da4a2c31e6e46f294bcb3cd163266386c86637
Kay <152438149+Sokpe@users.noreply.github.com>
2025-10-01 22:47:13 +0000

    Add files via upload

---
+++ django_blog/blog/views.py
@@ -0,0 +1,49 @@
+# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileUpdateForm, ProfilePictureUpdateForm
+from .models import Profile
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+
+
+
+
+@login_required
+def profile(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = Profile.objects.create(user=request.user)
+
+    if request.method == 'POST':
+        u_form = ProfileUpdateForm(request.POST, instance=request.user)
+        p_form = ProfilePictureUpdateForm(request.POST, request.FILES, instance=profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('profile')
+    else:
+        u_form = ProfileUpdateForm(instance=request.user)
+        p_form = ProfilePictureUpdateForm(instance=profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'blog/profile.html', context)
