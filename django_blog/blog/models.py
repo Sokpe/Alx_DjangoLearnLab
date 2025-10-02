8f2dfa795fefaf6a5fa697b36e4b43ceee603b14
Kay <152438149+Sokpe@users.noreply.github.com>
2025-10-02 18:30:30 +0000

    Add files via upload

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
-
 from django.contrib.auth.models import User
 
 class Post(models.Model):
@@ -10,6 +8,26 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pictures', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
