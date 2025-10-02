7d53800c687dfbcd9bc9702e937fb86d3e811e03
Kay <152438149+Sokpe@users.noreply.github.com>
2025-10-02 10:55:55 +0000

    Add files via upload

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('new/', views.PostCreateView.as_view(), name='post_create'),
+    path('<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_update'),
+    path('<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
     
 ]
-
