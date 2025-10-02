5085c0f6e3c616fddc309a537e93f095f5992c08
Kay <152438149+Sokpe@users.noreply.github.com>
2025-10-02 23:16:43 +0000

    Add files via upload

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -41,5 +41,4 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-
         return f'Comment by {self.author.username} on {self.post.title}'
