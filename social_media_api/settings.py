09091549cc3b0d1a22e61db1af761ee2bc9283e8
Kay <152438149+Sokpe@users.noreply.github.com>
2025-10-12 20:17:04 +0000

    Update settings.py

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'accounts',
     'rest_framework.authtoken',
+    'posts'
 ]
 # Custom user model
 AUTH_USER_MODEL = 'accounts.Customuser'
@@ -133,3 +134,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
