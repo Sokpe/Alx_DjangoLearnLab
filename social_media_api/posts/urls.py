d80ab6f0d3c4b67b776138dd524c0575a0d04cc5
Kay <152438149+Sokpe@users.noreply.github.com>
2025-10-13 14:00:24 +0000

    Add files via upload

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework import routers
 from .views import PostViewSet, CommentViewSet
 from .views import FeedView
+from .views import LikePostView, UnlikePostView
 
 router = routers.DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -19,4 +20,9 @@ urlpatterns = [
 
 urlpatterns = [
     path('feed/', FeedView.as_view(), name='feed'),
+]
+
+urlpatterns = [
+    path('<int:pk>/like/', LikePostView.as_view(), name='like_post'),
+    path('<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike_post'),
 ]
